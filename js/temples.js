19625064e3d7da944cd6bde3a98bd0910ca96254
SatoshiKenn <guevara.kennette@gmail.com>
2022-04-06 13:24:04 -0400

    temple js file update

--- js/temples.js
+++ js/temples.js
@@ -15,17 +15,25 @@ function displayTemple(temple) {
 
   let logo = document.createElement("img");
   let h2 = document.createElement("h2");
-  let p = document.createElement("p");
+  let address = document.createElement("p");
+  let services = document.createElement("p");
+  let telephone = document.createElement("p");
+  let ordinances = document.createElement("p");
+  let sessions = document.createElement("p");
+  let closures = document.createElement("p");
+  let history = document.createElement("p");
+  let a = document.createElement("a");
+
 
   h2.textContent = `${temple.name}`;
-  p.innerHTML = `<strong>Address:</strong> ${temple.address}<br/>
-    <strong>Services:</strong> ${temple.services}<br/>
-    <strong>Telephone:</strong> ${temple.phone}</br>
-    <strong>History:</strong> ${temple.history}</br>
-    <strong>Ordinances schedule:</strong> ${temple.ordinanceS}</br>
-    <strong>Session schedule:</strong> ${temple.sessionS}</br>
-    <strong>Closures:</strong> ${temple.closure}`;
-
+    
+  address.innerHTML = `<strong>Address:</strong> ${temple.address}`;
+  services.innerHTML = `<strong>Services:</strong> ${temple.services}`;
+  telephone.innerHTML = `<strong>Telephone:</strong> ${temple.phone}`;
+  ordinances.innerHTML = `<strong>Ordinances schedule:</strong> ${temple.ordinanceS}`;
+  sessions.innerHTML = `<strong>Session schedule:</strong> ${temple.sessionS}`;
+  closures.innerHTML = `<strong>Closures:</strong> ${temple.closure}`;
+  history.innerHTML = `<strong>History:</strong> ${temple.history}`;
   logo.setAttribute("src", temple.imageurl);
   logo.setAttribute("alt", `${temple.name}'s logo`);
   logo.setAttribute("loading", "lazy");
@@ -33,7 +41,14 @@ function displayTemple(temple) {
 
   card.appendChild(logo);
   card.appendChild(h2);
-  card.appendChild(p);
+  card.appendChild(address);
+  card.appendChild(services);
+  card.appendChild(telephone);
+  card.appendChild(ordinances);
+  card.appendChild(sessions);
+  card.appendChild(closures);
+  card.appendChild(history);
+
 
   document.querySelector("div.cards").appendChild(card);
 }
