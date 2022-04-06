4c0810cdd19eae3a558f7fc822ae53baa8f2efe7
SatoshiKenn <guevara.kennette@gmail.com>
2022-04-06 13:41:24 -0400

    base css update and temple js update

--- js/temples.js
+++ js/temples.js
@@ -13,36 +13,40 @@ function displayTemple(temple) {
   let card = document.createElement("section");
   card.setAttribute("class", "bcard");
 
-  let logo = document.createElement("img");
+  let templeimg = document.createElement("img");
   let h2 = document.createElement("h2");
   let address = document.createElement("p");
   let services = document.createElement("p");
+  let atelephone = document.createElement("a");
   let telephone = document.createElement("p");
   let ordinances = document.createElement("p");
   let sessions = document.createElement("p");
   let closures = document.createElement("p");
   let history = document.createElement("p");
-  let a = document.createElement("a");
 
 
   h2.textContent = `${temple.name}`;
     
   address.innerHTML = `<strong>Address:</strong> ${temple.address}`;
   services.innerHTML = `<strong>Services:</strong> ${temple.services}`;
-  telephone.innerHTML = `<strong>Telephone:</strong> ${temple.phone}`;
+  atelephone.setAttribute("href", `tel:${temple.phone.substring(1)}`)
+  telephone.innerHTML = `<strong>Telephone: </strong>`;
   ordinances.innerHTML = `<strong>Ordinances schedule:</strong> ${temple.ordinanceS}`;
   sessions.innerHTML = `<strong>Session schedule:</strong> ${temple.sessionS}`;
   closures.innerHTML = `<strong>Closures:</strong> ${temple.closure}`;
   history.innerHTML = `<strong>History:</strong> ${temple.history}`;
-  logo.setAttribute("src", temple.imageurl);
-  logo.setAttribute("alt", `${temple.name}'s logo`);
-  logo.setAttribute("loading", "lazy");
-  logo.setAttribute("class", "blogo");
+  atelephone.innerHTML = `${temple.phone}`
 
-  card.appendChild(logo);
+  templeimg.setAttribute("src", temple.imageurl);
+  templeimg.setAttribute("alt", `${temple.name}'s logo`);
+  templeimg.setAttribute("loading", "lazy");
+  templeimg.setAttribute("class", "timg");
+
+  card.appendChild(templeimg);
   card.appendChild(h2);
   card.appendChild(address);
   card.appendChild(services);
+  telephone.appendChild(atelephone)
   card.appendChild(telephone);
   card.appendChild(ordinances);
   card.appendChild(sessions);
